0216b3f6d0225e138a4cdb6261b3655b9cdd542c
einius99 <einius321@gmail.com>
2018-10-03 19:47:45 +0300

    nuu

--- UI/UI/Program.cs
+++ UI/UI/Program.cs
@@ -17,6 +17,7 @@ namespace UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            int a=0;
         }
     }
 }
