71295e5cd5be2e92e32486d6b5251e88a5284690
Ieva Vizgirdaitė <ievaviz92@gmail.com>
2018-10-14 17:13:31 +0300

    ...

--- UI/UI/User.cs
+++ UI/UI/User.cs
@@ -8,12 +8,14 @@ namespace UI
 {
     class User
     {
-        public User(string firstName, string lastName)
+        public User(string firstName, string lastName, string information)
         {
             FirstName = firstName;
             LastName = lastName;
+            Information = information;
         }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Information { get; set; }
     }
 }
