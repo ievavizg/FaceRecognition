3ea91cc71021262d513adad1c8d6fa0e2d5dcb86
Ieva Vizgirdaitė <ievaviz92@gmail.com>
2018-10-14 14:20:44 +0300

    User class

---
+++ UI/UI/ReadFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class ReadFiles
+    {
+        public void readKeyFile(ref apiInfo apiInformation, string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            apiInformation.apiKey = lines[0];
+            apiInformation.apiLoc = lines[1];
+        }
+    }
+}
